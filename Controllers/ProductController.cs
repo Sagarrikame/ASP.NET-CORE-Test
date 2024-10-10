dedb3d64978e7efe9e9c2c66447d3e02bd5b7f0c
Sagarrikame <128614691+Sagarrikame@users.noreply.github.com>
2024-10-10 17:11:08 +0530

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using QHRMProject.Models;
+
+namespace QHRMProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductController(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _productRepository.GetAllProducts();
+            return View(products);
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            _productRepository.AddProduct(product);
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var product = _productRepository.GetProductById(id);
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (!ModelState.IsValid)
+                return View(product);
+
+            _productRepository.UpdateProduct(product);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _productRepository.GetProductById(id);
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult ConfirmDelete(int id)
+        {
+            _productRepository.DeleteProduct(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Data/ProductRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using QHRMProject.Models;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+public class ProductRepository
+{
+    private readonly string connectionString;
+
+    public ProductRepository()
+    {
+        connectionString = @"Data Source=DESKTOP-BU3COTI\SQLEXPRESS;Initial Catalog=ProductsDapper;Integrated Security=True";
+    }
+
+    public IDbConnection Connection => new SqlConnection(connectionString);
+
+    public IEnumerable<Product> GetAllProducts()
+    {
+        using (IDbConnection dbConnection = Connection)
+        {
+            string query = "SELECT * FROM products";
+            return dbConnection.Query<Product>(query).ToList();
+        }
+    }
+
+    public void AddProduct(Product product)
+    {
+        using (IDbConnection dbConnection = Connection)
+        {
+            product.Created = DateTime.Now;
+            string query = "INSERT INTO products (ProductName, Description, Created, Price) VALUES (@ProductName, @Description, @Created, @Price)";
+            dbConnection.Execute(query, product);
+        }
+    }
+
+
+    public Product GetProductById(int id)
+    {
+        using (IDbConnection dbConnection = Connection)
+        {
+            string query = "SELECT * FROM products WHERE SN = @SN";
+            var product = dbConnection.Query<Product>(query, new { SN = id }).FirstOrDefault();
+            return product ?? new Product();
+        }
+    }
+
+  
+    public void UpdateProduct(Product product)
+    {
+        using (IDbConnection dbConnection = Connection)
+        {
+            string query = "UPDATE products SET ProductName = @ProductName, Description = @Description, Price = @Price WHERE SN = @SN";
+            dbConnection.Execute(query, product);
+        }
+    }
+
+    public void DeleteProduct(int id)
+    {
+        using (IDbConnection dbConnection = Connection)
+        {
+            string query = "DELETE FROM products WHERE SN = @SN";
+            dbConnection.Execute(query, new { SN = id });
+        }
+    }
+}
